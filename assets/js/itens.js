c97628f2d5cd62479bedc1531ee8d478154ad344
Vincent-Encanzo <joaogh357@gmail.com>
2022-08-01 20:40:31 -0300

    Subindo para o GitHub

---
+++ assets/js/itens.js
@@ -0,0 +1,91 @@
+let itensJson = [
+    {
+        id:1,
+        name:'Força do Vendaval', 
+        img:'../assets/images/item.png', 
+        price:3400, 
+        attributes:{
+            active: '<b>Ativo - Erupção das Brumas:</b> Avança na direção-alvo, disparando três projéteis no inimigo com a Vida mais baixa que estiver próximo ao destino do avanço (priorizando Campeões). Causa um total de 180 - 220 (Nv. 1 - 18) (+45% de Dano de Ataque adicional) de Dano mágico, aumentado em até 50% contra alvos com a Vida baixa (90s de Tempo de Recarga).',
+            passive: '<b>Passivo Mítico: +3 de Velocidade de Movimento para cada item lendário.</b>'
+        }, 
+        description:`+60 Dano de Ataque <br/> +20% Velocidade de Ataque <br/> +20% Acerto Crítico `,
+        datakey: 0
+    },
+
+    {
+        id:2,
+        name:'Gume do Infinito', img:'../assets/images/item02.png', 
+        price:3400,
+        attributes:{
+            active: '',
+            passive: '<b>Perfeição</b>: Se você tiver ao menos Chance de Acerto Crítico 65% de Chance de Acerto Crítico, receberá Dano de Acerto Crítico 35% de Dano de Acerto Crítico',
+        },
+        description:'+70 Dano de Ataque <br/> +20% Acerto Crítico',
+        datakey: 1
+    },
+
+    {
+        id:3, 
+        name:'Dente de Na´Shor', 
+        img:'../assets/images/item03.png', 
+        price:3000, 
+        attributes:{
+            active: '',
+            passive: '<b>Mordida Icathiana:</b> Ataques causam 15 + 20% de Poder de Habilidade como dano mágico ao contato',
+        },
+        description:'+100 Poder de Habilidade <br/> +50% Velocidade de Ataque',
+        datakey: 2
+    },
+
+    {
+        id:4, 
+        name:'Tormenta de Luden', 
+        img:'../assets/images/item04.png', 
+        price:3200, 
+        attributes:{
+            active: '',
+            passive: '<b>Eco:</b> causar dano a um inimigo com Habilidades causa 100 +10% de Poder de Habilidade como Dano Mágico adicional ao alvo e a 3 inimigos próximos, além de conceder a você 15% de Velocidade de Movimento por 2s (10s de Tempo de Recarga). Causar dano a Campeões com Habilidades reduz o Tempo de Recarga do item em 0.5s, até um máximo de 3s por conjuração. <br><br> <b>Passivo Mítico: concede a todos os outros itens Lendários 5 de Penetração Mágica.</b>',
+        }, 
+        description:'+80 Poder de Habilidade <br/> +6 Pen. Mágica <br/> +600 Mana <br/> +20 Aceleração de Habilidade',
+        datakey: 3
+    },
+
+    {
+        id:5, 
+        name:'Glacieterno', 
+        img:'../assets/images/item05.png', 
+        price:3200, 
+        attributes:{
+            active: '<b>Ativo – Geleira:</b> causa 100 + 30% de Poder de Habilidade como Dano Mágico em cone e 65% de Lentidão aos inimigos por 1.5s. Inimigos no centro do cone são Enraizados (30s de Tempo de Recarga).',
+            passive: '<b>Passivo Mítico: concede a todos os outros itens Lendários 15 de Poder de Habilidade.</b>',
+        }, 
+        description:'+70 Poder de Habilidade <br/> +250 Vida <br/> +600 Mana <br/> +20 Aceleração de Habilidade',
+        datakey: 4
+    },
+
+    {
+        id:6, 
+        name:'Perdição de Lich', 
+        img:'../assets/images/item06.png', 
+        price:3000, 
+        attributes:{
+            active: '',
+            passive: '<b>Lâmina Arcana:</b> depois de usar uma Habilidade, seu próximo Ataque é fortalecido e causa 150% do Dano de Ataque base + 40% de Poder de Habilidade como Dano Mágico adicional ao contato (2.5s de Tempo de Recarga).',
+        }, 
+        description:'+75 Poder de Habilidade <br/> +15 Aceleração de Habilidade <br/> +8% Velocidade de Movimento',
+        datakey: 5
+    },
+
+    {
+        id:7, 
+        name:'Regenerador de Pedra Lunar', 
+        img:'../assets/images/item07.png', 
+        price:2500, 
+        attributes:{
+            active: '',
+            passive: '<b>Graça Estrelada:</b> ao afetar Campeões com Ataques ou Habilidades em combate, restaura 70 de Vida ao aliado próximo mais ferido (2s de Tempo de Recarga). Cada segundo em combate com Campeões aumenta a cura e a Resistência do Escudo em 4% (máximo de 20%).<br><br> <b>Passivo Mítico: concede a todos os outros itens Lendários 10 de Vida aumentada na cura de Graça Estrelada.</b>',
+        }, 
+        description:'+40 Poder de Habilidade <br/> +200 Vida <br/> +20 Aceleração de Habilidade <br/> +100% Regen. de Mana Base',
+        datakey: 6
+    }
+];
---
+++ assets/js/script.js
@@ -0,0 +1,166 @@
+let modalQt = 1
+let modalKey = 0
+let cart = []
+
+const S = (el) => document.querySelector(el)
+const SA = (el) => document.querySelectorAll(el)
+
+itensJson.map((item, index) => {
+    let productItem = S('.models .product-item').cloneNode(true)
+
+    modalQt = 1
+
+    productItem.querySelector('.product-item--img img').src = item.img
+    productItem.querySelector('.product-item--price').innerHTML = `G$ ${item.price}`
+    productItem.querySelector('.product-item--name').innerHTML = item.name
+    productItem.querySelector('.product-item--desc').innerHTML = item.description
+
+    productItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault()
+        
+        productItem.setAttribute('data-key', index)
+        let key = e.target.closest('.product-item').getAttribute('data-key')
+        modalKey = key
+
+        S('.productBig img').src = itensJson[key].img
+        S('.productInfo h1').innerHTML = itensJson[key].name
+        S('.productInfo--desc').innerHTML = itensJson[key].description
+        S('.productInfo--actualPrice').innerHTML = `G$ ${itensJson[key].price}`
+        S('.productInfo--active').innerHTML = itensJson[key].attributes.active
+        S('.productInfo--passive').innerHTML = itensJson[key].attributes.passive
+
+        console.log(key)
+        S('.productWindowArea').style.opacity = 0
+        S('.productWindowArea').style.display = 'flex'
+        setTimeout(() => {
+            S('.productWindowArea').style.opacity = 1
+        }, 200)
+
+    })
+
+    S('.productInfo--qt').innerHTML = modalQt
+    S('.product-area').append(productItem)
+})
+
+let closeModal = () => {
+    S('.productWindowArea').style.opacity = 0
+    setTimeout(() => {
+        S('.productWindowArea').style.display = 'none'
+    }, 500)
+}
+
+SA('.productInfo--cancelMobileButton, .productInfo--cancelButton').forEach((item) => {
+    item.addEventListener('click', closeModal)
+})
+
+S('.productInfo--qtmenos').addEventListener('click', () => {
+    if (modalQt > 1){
+        modalQt--
+        S('.productInfo--qt').innerHTML = modalQt
+    }
+})
+
+S('.productInfo--qtmais').addEventListener('click', () => {
+    modalQt++
+    S('.productInfo--qt').innerHTML = modalQt
+})
+
+S('.productInfo--addButton').addEventListener('click', () => {
+    let key = cart.findIndex((item) => {
+        return item
+    })
+    if(key > -1){
+        cart[key].qt += modalQt
+    } else {
+        cart.push({
+            id: itensJson[modalKey].id,
+            qt: modalQt
+        })
+    }
+
+    updateCart()
+    closeModal()
+})
+
+S('.menu-openner').addEventListener('click', () => {
+    if (cart.length > 0){
+        S('aside').style.left = '0'
+    }
+})
+S('.menu-closer').addEventListener('click', () => {
+    S('aside').style.left = '100vw'
+})
+
+let updateCart = () => {
+    S('.menu-openner span').innerHTML = cart.length
+
+    if (cart.length > 0){
+        S('aside').classList.add('show')
+        S('.cart').innerHTML = ''
+
+        let subtotal = 0
+        let desconto = 0
+        let total = 0
+
+        for (let i in cart){
+            let productItem = itensJson.find((item)=>item.id == cart[i].id);
+            let cartItem = S('.models .cart--item').cloneNode(true)
+
+            subtotal += productItem.price * cart[i].qt
+
+            cartItem.querySelector('img').src = productItem.img;
+            cartItem.querySelector('.cart--item-nome').innerHTML = productItem.name
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', () => {
+                if(cart[i].qt > 1){
+                    cart[i].qt--
+                } else {
+                    cart.splice(i, 1)
+                }
+                updateCart()
+            })
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', () => {
+                cart[i].qt++
+                updateCart()
+            })
+
+            S('.cart').append(cartItem)
+
+        }
+
+        desconto = subtotal * 0.1
+        total = subtotal - desconto
+
+        S('.subtotal span:last-child').innerHTML = `G$ ${subtotal.toFixed(2)}`
+        S('.desconto span:last-child').innerHTML = `G$ ${desconto.toFixed(2)}`
+        S('.total span:last-child').innerHTML = `G$ ${total.toFixed(2)}`
+        S('.debit').innerHTML = `<b>G$ ${total}</b> de gold foram debitados de sua bag.`
+
+    } else {
+        S('aside').classList.remove('show')
+        S('aside').style.left = '100vw'
+    }
+}
+S('.cart--finalizar').addEventListener('click', () => {
+    S('.loading--screen').style.opacity = 0
+    S('.loading--screen').style.display = 'flex'
+    setTimeout(() => {
+        S('.loading--screen').style.opacity = 1
+    }, 200)
+
+    setTimeout(() => {
+        S('.loading--screen').style.display = 'none'
+    }, 5000)
+
+    setTimeout(() => {
+        S('.finished').style.opacity = 0
+        S('.finished').style.display = 'flex'
+        setTimeout(() => {
+            S('.finished').style.opacity = 1
+        }, 200)
+    }, 4500)
+
+})
+S('.reload').addEventListener('click', () => {
+    document.location.reload(true)
+})
